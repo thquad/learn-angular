01850743a2e8755fab72dedfd6d88d827aa4065b
Thomas Quandt <thquad@gmail.com>
2024-06-13 09:32:54 +0200

    setup angular 17

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'learn' title`, () => {
+  it(`should have the 'learn-angular' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('learn');
+    expect(app.title).toEqual('learn-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, learn');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, learn-angular');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,13 @@
-import { animate, state, style, transition, trigger, useAnimation } from '@angular/animations';
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
-import { fadeInFromRight, fadeOutToRight } from './animations/animations';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
-  animations: [
-    trigger('myAnimation', [
-      transition(':enter', [
-        useAnimation(fadeInFromRight,{
-          params:{
-            duration: '400ms',
-            easing:'ease-out'
-          }
-        })
-      ]),
-      transition(':leave', [
-        useAnimation(fadeOutToRight)
-      ])
-    ])
-  ]
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'learn';
-  visibility = true;
-
-  print() {
-    window.print();
-  }
-
-  toggleVisibility() {
-    this.visibility = !this.visibility;
-  }
+  title = 'learn-angular';
 }
