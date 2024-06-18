0b3f44739a561f6d98edccb3023a2e190f30f11a
Thomas Quandt <thquad@gmail.com>
2024-06-18 10:21:35 +0200

    adjust todo widget

--- src/app/component/todo/todo-widget/todo-widget.component.ts
+++ src/app/component/todo/todo-widget/todo-widget.component.ts
@@ -1,19 +1,19 @@
-import { Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { FormBuilder, FormControl, ReactiveFormsModule } from '@angular/forms';
 import { TodoInputComponent } from '../todo-input/todo-input.component';
 import { TodoListComponent } from '../todo-list/todo-list.component';
 import { Store } from '@ngrx/store';
-import { typePropertyIsNotAllowedInProps } from '@ngrx/store/src/models';
 import { TodoActions } from '../../../state/todo/todo.actions';
 import { selectTodoList } from '../../../state/todo/todo.reducer';
 import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-todo-widget',
+  templateUrl: './todo-widget.component.html',
+  styleUrl: './todo-widget.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
   imports: [ReactiveFormsModule, TodoInputComponent, TodoListComponent, AsyncPipe],
-  templateUrl: './todo-widget.component.html',
-  styleUrl: './todo-widget.component.scss'
 })
 export class TodoWidgetComponent {
   fb = inject(FormBuilder);
