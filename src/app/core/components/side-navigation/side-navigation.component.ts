1a16de160f29f93424d20471c4b1e657e69fcb5e
Thomas Quandt <thquad@gmail.com>
2024-07-25 09:19:51 +0200

    move stuff around and add active side nav items

--- src/app/core/components/side-navigation/side-navigation.component.ts
+++ src/app/core/components/side-navigation/side-navigation.component.ts
@@ -1,13 +1,13 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angular/core';
 import { NAVIGATION } from '../../../shared/model/path.model';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterModule } from '@angular/router';
 import { MatDivider } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-side-navigation',
   standalone: true,
-  imports: [RouterLink, MatDivider, MatButtonModule],
+  imports: [RouterModule, MatDivider, MatButtonModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './side-navigation.component.html',
   styleUrl: './side-navigation.component.scss'
